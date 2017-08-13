15c58189d759665212771b4812e064808f60ee4d
jurgenz <jurgenz@macs-MacBook-Pro.local>
2017-08-13 20:50:07 +0300

    update

--- src/CmdList.js
+++ src/CmdList.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import "./styles/CmdList.css";
 
-const CmdList = ({commands}) => {
-  const blueSpan = param => <span className="blueSpan">{'{'}{param}{'}'}</span>
+const CmdList = ({ commands }) => {
+  const blueSpan = param => <span className="blueSpan">{`{${param}}`}</span>
   const makeReply = (cmd) => {
     const paramIndex = cmd.indexOf('{param}')
     const nickIndex = cmd.indexOf('{nick}');
-    const cmdToReturn = cmd.split('{param}');
+    const urlParam = cmd.indexOf('{urlParam}') >= 0;
     // console.log(cmd2);
-    if (paramIndex === -1) {
+    if (paramIndex === -1 && !urlParam) {
       return cmd;
     }
+    // TODO:
+    if (urlParam) {
+      cmd = cmd.split('{urlParam}');
+      return (
+        <span>
+          {cmd[0]}
+          {blueSpan('urlParam')}
+          {cmd[1]}
+        </span>
+        
+      )
+    }
+
+    const cmdToReturn = cmd.split('{param}');
     return (
       <span>
         {cmdToReturn[0]}
@@ -24,10 +38,10 @@ const CmdList = ({commands}) => {
     return (
       <div key={`list_${i}`} className="cmdListItem">
         <span className="cmdName">
-          Command: <span style={{color: '#777'}}>{cmd.name}</span>
+          Command: <span style={{ color: '#777' }}>{cmd.name}</span>
         </span>
         <span className="cmdValue">
-          <span style={{fontWeight: 500, marginBottom: 'px'}}>Reply: </span>
+          <span style={{ fontWeight: 500, marginBottom: 'px' }}>Reply: </span>
           <span>{makeReply(cmd.value)}</span>
         </span>
       </div>
